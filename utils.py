a44b3950f36819a821ad6f94e2d717119de1d89b
mohammed imtiajul islam <mdimtiazulislam50@gmail.com>
2021-09-06 17:45:46 +0600

    initial commit

---
+++ utils.py
@@ -0,0 +1,53 @@
+import random
+
+from pygame import Color
+import pygame
+from pygame.image import load
+from pygame.math import Vector2
+from pygame.mixer import Sound
+
+pygame.mixer.init()
+pygame.mixer.music.load(r"D:\pythonUniverse\galaxiOfProgrammingHeros\pythonProject\myAsteroidGame\space_rocks\assets\sounds\background.wav")
+pygame.mixer.music.play()
+
+def load_sprite(name, with_alpha=True):
+    path = fr"D:\pythonUniverse\galaxiOfProgrammingHeros\pythonProject\myAsteroidGame\space_rocks\assets\sprites\{name}.png"
+    loaded_sprite = load(path)
+
+    if with_alpha:
+        return loaded_sprite.convert_alpha()
+    else:
+        return loaded_sprite.convert()
+
+
+def load_sound(name):
+    path = fr"D:\pythonUniverse\galaxiOfProgrammingHeros\pythonProject\myAsteroidGame\space_rocks\assets\sounds\{name}.wav"
+    return Sound(path)
+
+
+def wrap_position(position, surface):
+    x, y = position
+    w, h = surface.get_size()
+    return Vector2(x % w, y % h)
+
+
+def get_random_position(surface):
+    return Vector2(
+        random.randrange(surface.get_width()),
+        random.randrange(surface.get_height()),
+    )
+
+
+def get_random_velocity(min_speed, max_speed):
+    speed = random.randint(min_speed, max_speed)
+    angle = random.randrange(0, 360)
+    return Vector2(speed, 0).rotate(angle)
+
+
+def print_text(surface, text, font, color=Color("tomato")):
+    text_surface = font.render(text, False, color)
+
+    rect = text_surface.get_rect()
+    rect.center = Vector2(surface.get_size()) / 2
+
+    surface.blit(text_surface, rect)
